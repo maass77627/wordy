108d3d323dad7d14202d94e80314e85d2269eef0
kelly_maass <kmaass77627@gmail.com>
2020-06-15 02:29:27 -0500

    changes to game

--- src/index.js
+++ src/index.js
@@ -1,113 +1,104 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-    
-  })
+//, => {
+//     console.log('DOM fully loaded and parsed');
+   /// startGame()
+ // })
 
-  var myGamePiece;
-  var myObstacle;
-  var myBackground;
-  var myObstacleTwo;
- // var myScore;
-  function startGame() {
-      myGamePiece = new component(70, 70, "scottie.png", 240, 230, "image");
-     // myScore = new component("30px", "Consolas", "black", 280, 200, "text");
-      myObstacle = new component(60, 60, "cat.png", 0, 130, "image");
-      myObstacleTwo = new component(50, 50, "catone.png", 0, 70, "image");
-      myBackground = new component(656, 270, "flower.jpg", 0, 0, "image");
-      myGameArea.start();
-  }
+ let canvas, ctx
   
-  var myGameArea = {
-      canvas : document.createElement("canvas"),
-      start : function() {
-          this.canvas.width = 480;
-          this.canvas.height = 270;
-          this.context = this.canvas.getContext("2d");
-          document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-          this.frameNo = 0;
-          this.interval = setInterval(updateGameArea, 20);
-          },
-      clear : function() {
-          this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      },
-      stop : function() {
-          clearInterval(this.interval);
-      }
-  }
-  
-  function component(width, height, color, x, y, type) {
-      this.type = type;
-      if (type == "image") {
-          this.image = new Image();
-          this.image.src = color;
-      }
-      this.width = width;
-      this.height = height;
-      this.speedX = 0;
-      this.speedY = 0;    
-      this.x = x;
-      this.y = y;    
-      this.update = function() {
-          ctx = myGameArea.context;
-          // if (this.type == "text") {
-          //   ctx.font = this.width + " " + this.height;
-          //   ctx.fillStyle = color;
-          //   ctx.fillText(this.text, this.x, this.y);
-          // } else {
-            
-          
-          if (type == "image") {
-              ctx.drawImage(this.image, 
-                  this.x, 
-                  this.y,
-                  this.width, this.height);
-          } else {
-              ctx.fillStyle = color;
-              ctx.fillRect(this.x, this.y, this.width, this.height);
-          }}
-          
-      }
-      this.newPos = function() {
-          this.x += this.speedX;
-          this.y += this.speedY;
-                    
-      }
-  }
-  
-  function updateGameArea() {
-      myGameArea.clear();
-     // myGameArea.frameNo += 1;
-      myBackground.newPos();
-      myBackground.update();
-     
-      myObstacle.x += 1;
-      myObstacle.update();
-      myObstacleTwo.x += 3;
-      myObstacleTwo.update();
-      myGamePiece.newPos();
-      myGamePiece.update();
-  }
-  
-  function moveup() {
-      myGamePiece.speedY = -1; 
-  }
-  
-  function movedown() {
-      myGamePiece.speedY = 1; 
-  }
-  
-  function moveleft() {
-      myGamePiece.speedX = -1; 
-  }
-  
-  function moveright() {
-      myGamePiece.speedX = 1; 
-  }
-  
-  function clearmove() {
-      myGamePiece.speedX = 0; 
-      myGamePiece.speedY = 0; 
-  }
-    
+
+
+  function init () {
+    canvas = document.getElementById('myCanvas')
+    ctx = canvas.getContext('2d')
 
     
+
+    ctx.beginPath()
+ctx.fillStyle = 'gold'
+ctx.arc(200, 300, 15, 0.1 * Math.PI, 1.85 * Math.PI)
+ctx.lineTo(200, 300)
+ctx.fill()
+
+// filled, outlined square X: 200, Y: 35, width/height 50
+ctx.beginPath()
+ctx.strokeStyle = 'red'
+ctx.fillStyle = 'blue'
+ctx.lineWidth = 5
+ctx.rect(200, 35, 50, 50)
+ctx.fill()
+ctx.stroke()
+
+ctx.font = '48px serif';
+    ctx.fillText('Hello world', 10, 50);
+
+    var FPS = 30;
+setInterval(function() {
+  update();
+  draw();
+}, 1000/FPS);
+
+var textX = 50;
+var textY = 50;
+
+function update() {
+  textX += 1;
+  textY += 1;
+}
+
+function draw() {
+    ctx.clearRect(0, 0, 700, 500);
+    ctx.fillStyle = "#000";
+    ctx.fillText("Sup Bro!", textX, textY);
+    player.draw();
+  }
+
+  var player = {
+    image: "scottie.png",
+    x: 220,
+    y: 270,
+    }
+  };
+
+
+ 
+  var ballRadius = 10;
+  var x = canvas.width/2;
+  var y = canvas.height-30;
+  var dx = 2;
+  var dy = -2;
+  
+  function drawBall() {
+      ctx.beginPath();
+      ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+      ctx.fillStyle = "#0095DD";
+      ctx.fill();
+      ctx.closePath();
+  }
+  
+  function draw() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      drawBall();
+      
+      if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+          dx = -dx;
+      }
+      if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+          dy = -dy;
+      }
+      
+      x += dx;
+      y += dy;
+  }
+  
+  setInterval(draw, 10);
+
+ctx.restore()
+  
+
+  
+
+ 
+
+
+
+  document.addEventListener('DOMContentLoaded', init)
